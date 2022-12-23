9e769dcbefd40d70f85f5a3f705d4559295c6479
Hanifa Rifnaz <hanifarifnazz@gmail.com>
2022-12-23 19:29:50 +0530

    Designed Splash Screen

---
+++ LockHood/frmManHome.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace LockHood
+{
+    partial class frmManHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmManHome));
+            this.pnlBody = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnEmployee = new System.Windows.Forms.Button();
+            this.btnSupervisors = new System.Windows.Forms.Button();
+            this.btnHeads = new System.Windows.Forms.Button();
+            this.btnTasks = new System.Windows.Forms.Button();
+            this.btnDash = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblDepart = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlHead = new System.Windows.Forms.Panel();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_X = new System.Windows.Forms.Button();
+            this.pnlBody.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlHead.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBody
+            // 
+            this.pnlBody.Controls.Add(this.panel4);
+            this.pnlBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBody.Location = new System.Drawing.Point(225, 75);
+            this.pnlBody.Name = "pnlBody";
+            this.pnlBody.Size = new System.Drawing.Size(1075, 625);
+            this.pnlBody.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 600);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1075, 25);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.btnReports);
+            this.panel1.Controls.Add(this.btnLogout);
+            this.panel1.Controls.Add(this.btnEmployee);
+            this.panel1.Controls.Add(this.btnSupervisors);
+            this.panel1.Controls.Add(this.btnHeads);
+            this.panel1.Controls.Add(this.btnTasks);
+            this.panel1.Controls.Add(this.btnDash);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(225, 625);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnReports
+            // 
+            this.btnReports.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnReports.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReports.FlatAppearance.BorderSize = 0;
+            this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReports.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReports.ForeColor = System.Drawing.Color.White;
+            this.btnReports.Location = new System.Drawing.Point(0, 444);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(225, 50);
+            this.btnReports.TabIndex = 8;
+            this.btnReports.Text = "Report";
+            this.btnReports.UseVisualStyleBackColor = false;
+            this.btnReports.Click += new System.EventHandler(this.btnReports_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnLogout.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.Color.Brown;
+            this.btnLogout.Location = new System.Drawing.Point(0, 550);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(225, 50);
+            this.btnLogout.TabIndex = 7;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // btnEmployee
+            // 
+            this.btnEmployee.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnEmployee.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEmployee.FlatAppearance.BorderSize = 0;
+            this.btnEmployee.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmployee.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployee.ForeColor = System.Drawing.Color.White;
+            this.btnEmployee.Location = new System.Drawing.Point(0, 394);
+            this.btnEmployee.Name = "btnEmployee";
+            this.btnEmployee.Size = new System.Drawing.Size(225, 50);
+            this.btnEmployee.TabIndex = 6;
+            this.btnEmployee.Text = "Employee";
+            this.btnEmployee.UseVisualStyleBackColor = false;
+            this.btnEmployee.Click += new System.EventHandler(this.btnEmployee_Click);
+            // 
+            // btnSupervisors
+            // 
+            this.btnSupervisors.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnSupervisors.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSupervisors.FlatAppearance.BorderSize = 0;
+            this.btnSupervisors.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSupervisors.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSupervisors.ForeColor = System.Drawing.Color.White;
+            this.btnSupervisors.Location = new System.Drawing.Point(0, 344);
+            this.btnSupervisors.Name = "btnSupervisors";
+            this.btnSupervisors.Size = new System.Drawing.Size(225, 50);
+            this.btnSupervisors.TabIndex = 5;
+            this.btnSupervisors.Text = "Supervisors";
+            this.btnSupervisors.UseVisualStyleBackColor = false;
+            this.btnSupervisors.Click += new System.EventHandler(this.btnSupervisors_Click);
+            // 
+            // btnHeads
+            // 
+            this.btnHeads.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnHeads.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHeads.FlatAppearance.BorderSize = 0;
+            this.btnHeads.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHeads.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHeads.ForeColor = System.Drawing.Color.White;
+            this.btnHeads.Location = new System.Drawing.Point(0, 294);
+            this.btnHeads.Name = "btnHeads";
+            this.btnHeads.Size = new System.Drawing.Size(225, 50);
+            this.btnHeads.TabIndex = 4;
+            this.btnHeads.Text = "Heads";
+            this.btnHeads.UseVisualStyleBackColor = false;
+            this.btnHeads.Click += new System.EventHandler(this.btnHeads_Click);
+            // 
+            // btnTasks
+            // 
+            this.btnTasks.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnTasks.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTasks.FlatAppearance.BorderSize = 0;
+            this.btnTasks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTasks.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTasks.ForeColor = System.Drawing.Color.White;
+            this.btnTasks.Location = new System.Drawing.Point(0, 244);
+            this.btnTasks.Name = "btnTasks";
+            this.btnTasks.Size = new System.Drawing.Size(225, 50);
+            this.btnTasks.TabIndex = 3;
+            this.btnTasks.Text = "Tasks";
+            this.btnTasks.UseVisualStyleBackColor = false;
+            this.btnTasks.Click += new System.EventHandler(this.btnTasks_Click);
+            // 
+            // btnDash
+            // 
+            this.btnDash.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnDash.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDash.FlatAppearance.BorderSize = 0;
+            this.btnDash.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDash.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDash.ForeColor = System.Drawing.Color.White;
+            this.btnDash.Location = new System.Drawing.Point(0, 194);
+            this.btnDash.Name = "btnDash";
+            this.btnDash.Size = new System.Drawing.Size(225, 50);
+            this.btnDash.TabIndex = 2;
+            this.btnDash.Text = "Dashboard";
+            this.btnDash.UseVisualStyleBackColor = false;
+            this.btnDash.Click += new System.EventHandler(this.btnDash_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel5.Location = new System.Drawing.Point(0, 600);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(225, 25);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel2.Controls.Add(this.lblDepart);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(225, 194);
+            this.panel2.TabIndex = 0;
+            // 
+            // lblDepart
+            // 
+            this.lblDepart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDepart.AutoSize = true;
+            this.lblDepart.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepart.ForeColor = System.Drawing.Color.DimGray;
+            this.lblDepart.Location = new System.Drawing.Point(78, 158);
+            this.lblDepart.Name = "lblDepart";
+            this.lblDepart.Size = new System.Drawing.Size(61, 17);
+            this.lblDepart.TabIndex = 3;
+            this.lblDepart.Text = "Factory1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(54, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Manager of";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(61, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnlHead
+            // 
+            this.pnlHead.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlHead.Controls.Add(this.btn_X);
+            this.pnlHead.Controls.Add(this.lblHeading);
+            this.pnlHead.Controls.Add(this.label1);
+            this.pnlHead.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHead.Location = new System.Drawing.Point(0, 0);
+            this.pnlHead.Name = "pnlHead";
+            this.pnlHead.Size = new System.Drawing.Size(1300, 75);
+            this.pnlHead.TabIndex = 3;
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Nirmala UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.ForeColor = System.Drawing.Color.White;
+            this.lblHeading.Location = new System.Drawing.Point(672, 19);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(157, 37);
+            this.lblHeading.TabIndex = 1;
+            this.lblHeading.Text = "Dashboard";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LockHood Pvt Ltd";
+            // 
+            // btn_X
+            // 
+            this.btn_X.BackColor = System.Drawing.Color.Brown;
+            this.btn_X.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_X.FlatAppearance.BorderSize = 0;
+            this.btn_X.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_X.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_X.ForeColor = System.Drawing.Color.White;
+            this.btn_X.Location = new System.Drawing.Point(1265, 0);
+            this.btn_X.Name = "btn_X";
+            this.btn_X.Size = new System.Drawing.Size(35, 35);
+            this.btn_X.TabIndex = 2;
+            this.btn_X.Text = "X";
+            this.btn_X.UseVisualStyleBackColor = false;
+            this.btn_X.Click += new System.EventHandler(this.btn_X_Click);
+            // 
+            // frmManHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 700);
+            this.Controls.Add(this.pnlBody);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlHead);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmManHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmManHome";
+            this.Load += new System.EventHandler(this.frmManHome_Load);
+            this.pnlBody.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlHead.ResumeLayout(false);
+            this.pnlHead.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBody;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnReports;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnEmployee;
+        private System.Windows.Forms.Button btnSupervisors;
+        private System.Windows.Forms.Button btnHeads;
+        private System.Windows.Forms.Button btnTasks;
+        private System.Windows.Forms.Button btnDash;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblDepart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnlHead;
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_X;
+    }
+}
---
+++ LockHood/frmManHome.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LockHood
+{
+    public partial class frmManHome : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-N7LQHOM;Initial Catalog=LockHood;Integrated Security=True");
+
+        public frmManHome()
+        {
+            InitializeComponent();
+        }
+
+        private void frmManHome_Load(object sender, EventArgs e)
+        {
+            loadadhomeform(new frmManagerDash());
+            //Current color
+            btnDash.BackColor = Color.White;
+            btnDash.ForeColor = Color.CornflowerBlue;
+
+            // Check the connection status & close/Open
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+            // Fill the Username Textbox
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("select Factory from manager", con);
+            da.Fill(dt);
+
+            lblDepart.Text = dt.Rows[0][0].ToString();
+        }
+        public void loadadhomeform(object Form)
+        {
+            if (this.pnlBody.Controls.Count > 0)
+                this.pnlBody.Controls.RemoveAt(0);
+            Form f = Form as Form;
+            f.TopLevel = false;
+            f.Dock = DockStyle.Fill;
+            this.pnlBody.Controls.Add(f);
+            this.pnlBody.Tag = f;
+            f.Show();
+        }
+
+        private void btnDash_Click(object sender, EventArgs e)
+        {
+            //btn Font Color
+            btnTasks.ForeColor = Color.White;
+            btnHeads.ForeColor = Color.White;
+            btnSupervisors.ForeColor = Color.White;
+            btnEmployee.ForeColor = Color.White;
+            btnReports.ForeColor = Color.White;
+
+            //btn Back Color
+            btnTasks.BackColor = Color.CornflowerBlue;
+            btnHeads.BackColor = Color.CornflowerBlue;
+            btnSupervisors.BackColor = Color.CornflowerBlue;
+            btnEmployee.BackColor = Color.CornflowerBlue;
+            btnReports.BackColor = Color.CornflowerBlue;
+
+            //Current color
+            btnDash.BackColor = Color.White;
+            btnDash.ForeColor = Color.CornflowerBlue;
+
+            loadadhomeform(new frmManagerDash());
+            lblHeading.Text = "Dashboard";
+        }
+
+        private void btnTasks_Click(object sender, EventArgs e)
+        {
+            //btn Font Color
+            btnDash.ForeColor = Color.White;
+            btnHeads.ForeColor = Color.White;
+            btnSupervisors.ForeColor = Color.White;
+            btnEmployee.ForeColor = Color.White;
+            btnReports.ForeColor = Color.White;
+
+            //btn Back Color
+            btnDash.BackColor = Color.CornflowerBlue;
+            btnHeads.BackColor = Color.CornflowerBlue;
+            btnSupervisors.BackColor = Color.CornflowerBlue;
+            btnEmployee.BackColor = Color.CornflowerBlue;
+            btnReports.BackColor = Color.CornflowerBlue;
+
+            //Current color
+            btnTasks.BackColor = Color.White;
+            btnTasks.ForeColor = Color.CornflowerBlue;
+
+            //loadadhomeform(new frmManagerDash());
+            lblHeading.Text = "Tasks";
+        }
+
+        private void btnHeads_Click(object sender, EventArgs e)
+        {
+            //btn Font Color
+            btnDash.ForeColor = Color.White;
+            btnTasks.ForeColor = Color.White;
+            btnSupervisors.ForeColor = Color.White;
+            btnEmployee.ForeColor = Color.White;
+            btnReports.ForeColor = Color.White;
+
+            //btn Back Color
+            btnDash.BackColor = Color.CornflowerBlue;
+            btnTasks.BackColor = Color.CornflowerBlue;
+            btnSupervisors.BackColor = Color.CornflowerBlue;
+            btnEmployee.BackColor = Color.CornflowerBlue;
+            btnReports.BackColor = Color.CornflowerBlue;
+
+            //Current color
+            btnHeads.BackColor = Color.White;
+            btnHeads.ForeColor = Color.CornflowerBlue;
+
+            //loadadhomeform(new frmManagerDash());
+            lblHeading.Text = " Department Heads";
+        }
+
+        private void btnSupervisors_Click(object sender, EventArgs e)
+        {
+            //btn Font Color
+            btnDash.ForeColor = Color.White;
+            btnTasks.ForeColor = Color.White;
+            btnHeads.ForeColor = Color.White;
+            btnEmployee.ForeColor = Color.White;
+            btnReports.ForeColor = Color.White;
+
+            //btn Back Color
+            btnDash.BackColor = Color.CornflowerBlue;
+            btnTasks.BackColor = Color.CornflowerBlue;
+            btnHeads.BackColor = Color.CornflowerBlue;
+            btnEmployee.BackColor = Color.CornflowerBlue;
+            btnReports.BackColor = Color.CornflowerBlue;
+
+            //Current color
+            btnSupervisors.BackColor = Color.White;
+            btnSupervisors.ForeColor = Color.CornflowerBlue;
+
+            //loadadhomeform(new frmManagerDash());
+            lblHeading.Text = "Supervisors";
+        }
+
+        private void btnEmployee_Click(object sender, EventArgs e)
+        {
+            //btn Font Color
+            btnDash.ForeColor = Color.White;
+            btnTasks.ForeColor = Color.White;
+            btnHeads.ForeColor = Color.White;
+            btnSupervisors.ForeColor = Color.White;
+            btnReports.ForeColor = Color.White;
+
+            //btn Back Color
+            btnDash.BackColor = Color.CornflowerBlue;
+            btnTasks.BackColor = Color.CornflowerBlue;
+            btnHeads.BackColor = Color.CornflowerBlue;
+            btnSupervisors.BackColor = Color.CornflowerBlue;
+            btnReports.BackColor = Color.CornflowerBlue;
+
+            //Current color
+            btnEmployee.BackColor = Color.White;
+            btnEmployee.ForeColor = Color.CornflowerBlue;
+
+            //loadadhomeform(new frmManagerDash());
+            lblHeading.Text = "Employees";
+        }
+
+        private void btnReports_Click(object sender, EventArgs e)
+        {
+            //btn Font Color
+            btnDash.ForeColor = Color.White;
+            btnTasks.ForeColor = Color.White;
+            btnHeads.ForeColor = Color.White;
+            btnSupervisors.ForeColor = Color.White;
+            btnEmployee.ForeColor = Color.White;
+
+            //btn Back Color
+            btnDash.BackColor = Color.CornflowerBlue;
+            btnTasks.BackColor = Color.CornflowerBlue;
+            btnHeads.BackColor = Color.CornflowerBlue;
+            btnSupervisors.BackColor = Color.CornflowerBlue;
+            btnEmployee.BackColor = Color.CornflowerBlue;
+
+            //Current color
+            btnReports.BackColor = Color.White;
+            btnReports.ForeColor = Color.CornflowerBlue;
+
+            //loadadhomeform(new frmManagerDash());
+            lblHeading.Text = "Generate Reports";
+        }
+
+        private void btn_X_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
